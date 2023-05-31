3b00c1d6698fdfb20bd385ad2688f73a0ffb616f
niuyuean <niuyuean99@gmail.com>
2023-05-31 20:51:59 +0800

    pan

--- handle.go
+++ handle.go
@@ -139,8 +139,9 @@ func batchProfile(w http.ResponseWriter, r *http.Request) {
 		//log.Println(data.Keys)
 		keys := data.Keys
 		pattern := data.Pattern
+		var local_result []string
 		if pattern == 0 {
-			var local_result []string
+
 			for _, lk := range keys {
 				if lv, ok := Local_emb[lk]; ok{
 					local_result = append(local_result, lv)
@@ -153,7 +154,6 @@ func batchProfile(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode(result)
 		} else if pattern == 1 {
 			//var local_data []int
-			var local_result []string
 			for i:=0;i<len(keys)/2;i++{
 				if lv, ok := Local_emb[keys[i]]; ok{
 					local_result = append(local_result, lv)
@@ -163,8 +163,9 @@ func batchProfile(w http.ResponseWriter, r *http.Request) {
 			tmp["keys"] = keys[len(keys)/2:]
 			newData, _ := json.Marshal(tmp)
 			if resp, err := http.Post(CubeIps[1]+"/DictService/seek", "application/json", bytes.NewReader(newData)); err == nil {
-				ioutil.ReadAll(resp.Body)
-				//fmt.Println(string(body))
+				body,_ := ioutil.ReadAll(resp.Body)
+
+				fmt.Println(string(body))
 			} else {
 				log.Println("error")
 			}
@@ -208,9 +209,75 @@ func batchProfile(w http.ResponseWriter, r *http.Request) {
 			//}
 			//wg.Wait()
 		} else if  pattern == 2{
-
+			var wg sync.WaitGroup
+			wg.Add(pattern)
+			for i:=0;i<len(keys)/2;i++{
+				if lv, ok := Local_emb[keys[i]]; ok{
+					local_result = append(local_result, lv)
+				}
+			}
+			for i:=0;i<pattern;i++ {
+				go func(i int) {
+					defer wg.Done()
+					tmp := make(map[string][]int)
+					tmp["keys"] = keys[:len(keys)/4]
+					newData, err := json.Marshal(tmp)
+					if err != nil {
+						log.Println(err)
+					} else {
+						//r.Body = ioutil.NopCloser(bytes.NewBuffer(newData))
+						//req, err := http.Post(remote.Host, bytes.NewReader(newData))
+						if resp, err := http.Post(CubeIps[i]+"/DictService/seek", "application/json", bytes.NewReader(newData)); err == nil {
+							ioutil.ReadAll(resp.Body)
+							//fmt.Println(string(body))
+						} else {
+							log.Println("error")
+						}
+					}
+				}(i)
+			}
+			wg.Wait()
+			result := make(map[string]interface{})
+			elapsed := time.Since(start)
+			result["time"] = elapsed
+			log.Println(result)
+			json.NewEncoder(w).Encode(result)
 		} else if  pattern == 3{
-
+			var wg sync.WaitGroup
+			wg.Add(12)
+			for i:=0;i<len(keys)/2;i++{
+				if lv, ok := Local_emb[keys[i]]; ok{
+					local_result = append(local_result, lv)
+				}
+			}
+			for i:=0;i<12;i++ {
+				go func(k int) {
+					defer wg.Done()
+					tmp := make(map[string][]int)
+					tmp["keys"] = keys[:len(keys)/6]
+					newData, err := json.Marshal(tmp)
+					//log.Println(newData)
+					if err != nil {
+						log.Println(err)
+					} else {
+						//r.Body = ioutil.NopCloser(bytes.NewBuffer(newData))
+						//req, err := http.Post(remote.Host, bytes.NewReader(newData))
+						log.Println(CubeIps[k]+"/DictService/seek")
+						if resp, err := http.Post(CubeIps[k]+"/DictService/seek", "application/json", bytes.NewReader(newData)); err == nil {
+							body,_ := ioutil.ReadAll(resp.Body)
+							fmt.Println(string(body))
+						} else {
+							log.Println("error")
+						}
+					}
+				}(i%4)
+			}
+			wg.Wait()
+			result := make(map[string]interface{})
+			elapsed := time.Since(start)
+			result["time"] = elapsed
+			log.Println(result)
+			json.NewEncoder(w).Encode(result)
 		} else {
 			return
 		}
